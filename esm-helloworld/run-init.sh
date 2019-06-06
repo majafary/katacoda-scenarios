d514e647380c5d13ded0fd91881d396ae5b43c43
Mansoor Jafary <Mansoor.Jafary@aexp.com>
2019-06-06 10:41:24 -0700

    testing out launch.sh

--- esm-helloworld/run-init.sh
+++ esm-helloworld/run-init.sh
@@ -1,2 +1,3 @@
+launch.sh
 hostname -I | tr ' ' '\n' | awk NF | awk '{print $1 " master"}' | tee -a /etc/hosts ; systemctl restart dnsmasq ; setenforce 0
 oc adm policy add-cluster-role-to-user cluster-admin admin
