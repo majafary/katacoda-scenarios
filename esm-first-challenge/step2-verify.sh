ca4a6a75eaafa84aa6a7ab23b6b72328f6465bc9
Vikash Chandra Mishra <Vikash.C.Mishra@aexp.com>
2019-06-27 01:16:17 -0700

    Added the result page

--- esm-first-challenge/step2-verify.sh
+++ esm-first-challenge/step2-verify.sh
@@ -1,3 +1,4 @@
 curl $(kubectl get service | grep 'hello-service' | awk '{print $3}') > test.txt
 
-(( $(cat test.txt | wc -w) == 2 )) && echo done
+(( $(cat test.txt | wc -w) == 2 )) && export pass=true
+echo done
---
+++ esm-first-challenge/step3.md
@@ -0,0 +1,1 @@
+The result is displayed in right hand side
